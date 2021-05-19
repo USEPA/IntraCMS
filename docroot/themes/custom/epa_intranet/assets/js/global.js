864b0c554b9e00df81770d8582bababa745485f0
jrockwei <john.rockweiler@cgifederal.com>
2021-05-19 12:10:49 -0500

    IN-136 Update Jira Issue Collector Configuration

--- docroot/themes/custom/epa_intranet/assets/js/global.js
+++ docroot/themes/custom/epa_intranet/assets/js/global.js
@@ -1,3 +1,20 @@
 /*!
   * global stuff - likely from bootstrap
   */
+
+window.ATL_JQ_PAGE_PROPS = {
+    "triggerFunction": function(showCollectorDialog) {
+//Requires that jQuery is available!
+        $("#altwdg-trigger").click(function(e) {
+            e.preventDefault();
+            showCollectorDialog();
+        });
+    },
+    fieldValues: {
+
+        recordWebInfo: '1', // field Name
+        recordWebInfoConsent: ['1'] // field Id
+
+    }
+
+};
