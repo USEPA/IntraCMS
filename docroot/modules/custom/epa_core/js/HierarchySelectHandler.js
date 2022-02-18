f812c5d0c0b241faab986d7602ad5857d17ee055
Jessica Dearie <jdearie@users.noreply.github.com>
2022-02-18 14:13:05 -0500

    Create HierarchySelectHandler.js

---
+++ docroot/modules/custom/epa_core/js/HierarchySelectHandler.js
@@ -0,0 +1,19 @@
+(function (Drupal, $) {
+   Drupal.behaviors.hierarchy_change = {
+     attach: function (context, settings) {
+         $(document).on('change', '.cshs-custom-select:last', function(event){
+           var selected_id = $('.cshs-custom-select').val();
+           // lookup corresponding drupal term and populate with metadata
+           $.get('/taxonomy_depth_lookup_standard/' + selected_id, function(data) {
+             if (data.owner && data.size) {
+               $('.all-windows-shares-metadata').show();
+               $('.cshs-custom-select-additions').text(data.owner + ' - ' + data.size + '_gb');
+             } else {
+               $('.all-windows-shares-metadata').hide();
+             }
+           });
+         })
+     }
+   };
+
+})(Drupal, jQuery);
