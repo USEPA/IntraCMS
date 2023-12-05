b49d7aee6f972329a0dcb769a14e3e3920e17462
Ryan P Letulle (CGI) <92539208+ryanletulle@users.noreply.github.com>
2023-12-05 14:02:10 -0600

    IN-2158 IN-2117 IN-2170

--- docroot/themes/custom/epa_intranet_2/assets/js/global.js
+++ docroot/themes/custom/epa_intranet_2/assets/js/global.js
@@ -27,3 +27,5 @@ jQuery("figure.caption article.align-center").parent().addClass('align-center');
 /* Update the anchor tag for form links. */
 var cleanURL = jQuery('#form-href-source a').prop('href');
 jQuery('#form-href-link').attr('href', cleanURL);
+
+jQuery('#block-epa-intranet-content table').addClass('usa-table');
