0cc96df34e6b958673bdd626ba0c6ce157df9a32
Letulle <ryan.letulle@cgifederal.com>
2025-05-12 14:03:36 -0500

    Tweak the theme to remove a javascript error.

--- docroot/themes/custom/epa_intranet_2/assets/js/datatableInitialization.js
+++ docroot/themes/custom/epa_intranet_2/assets/js/datatableInitialization.js
@@ -6,7 +6,7 @@
   // All the JavaScript for this file.
   Drupal.behaviors.initializeDatatables = {
     attach(context, settings) {
-      $('.datatable', context).once('init').addClass('usa-table').DataTable();
+      $('.datatable', context).addClass('usa-table').DataTable();
     }
   };
 })(jQuery, Drupal);
